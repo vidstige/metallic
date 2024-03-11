3a6547b2c4c46309f07052fb7758ca55ed386eaf
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-03-11 09:11:01 +0100

    stream video

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io::{self, Write}};
+
+type Resolution = (i32, i32);
+
+fn parse_resolution(s: &String) -> Resolution {
+    let mut parts = s.split("x");
+    let width = parts.next().unwrap().parse().unwrap();
+    let height = parts.next().unwrap().parse().unwrap();
+    (width, height)
+}
+
+fn area(resolution: Resolution) -> usize {
+    let (width, height) = resolution;
+    (width * height) as usize
+}
+
+fn main() -> io::Result<()>{
+    let resolution = parse_resolution(&env::var("RESOLUTION").unwrap_or("506x253".to_string()));
+    let buffer = vec![0; area(resolution) * 4];
+    std::io::stdout().write(&buffer)?;
+    Ok(())
 }
