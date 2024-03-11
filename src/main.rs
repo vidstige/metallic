88492b887f6b6f7065b287d1d69e6a73a0f11ba8
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-03-11 09:54:14 +0100

    draw pixels

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,37 @@ fn area(resolution: Resolution) -> usize {
     (width * height) as usize
 }
 
+struct Buffer {
+    resolution: Resolution,
+    pixels: Vec<u8>,
+}
+
+impl Buffer {
+    fn new(resolution: Resolution) -> Buffer {
+        Buffer { resolution: resolution, pixels: vec![0; area(resolution) * 4]}
+    }
+}
+
+fn pixel(target: &mut Buffer, x: i32, y: i32, color: u32) {
+    let (stride, _) = target.resolution;
+    let index = ((x + y * stride) * 4) as usize;
+    let bytes = color.to_le_bytes();
+    target.pixels[index..index + bytes.len()].copy_from_slice(&bytes);
+}
+
+fn render(target: &mut Buffer) {
+    let (width, height) = target.resolution;
+    for y in 0..height {
+        for x in 0..width {
+            pixel(target, x, y, 0xffcecece);
+        }
+    }
+}
+
 fn main() -> io::Result<()>{
     let resolution = parse_resolution(&env::var("RESOLUTION").unwrap_or("506x253".to_string()));
-    let buffer = vec![0; area(resolution) * 4];
-    std::io::stdout().write(&buffer)?;
+    let mut buffer = Buffer::new(resolution);
+    render(&mut buffer);
+    std::io::stdout().write(&buffer.pixels)?;
     Ok(())
 }
